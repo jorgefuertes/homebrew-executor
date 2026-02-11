40e6877029cc4c756e358ac9d4611736d60d20a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-11 10:05:20 +0000

    Update bottles for version 1.0.25

--- Formula/executor.rb
+++ Formula/executor.rb
@@ -11,4 +11,12 @@ class Executor < Formula
     url :stable
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
+
+  bottle do
+    root_url "https://github.com/jorgefuertes/executor/releases/download/v#{VERSION}"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0daeaf43b957974eae473199fc66267ab579812dfba33fa4e3981203c45cd327"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "0daeaf43b957974eae473199fc66267ab579812dfba33fa4e3981203c45cd327"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0daeaf43b957974eae473199fc66267ab579812dfba33fa4e3981203c45cd327"
+    sha256 cellar: :any_skip_relocation, sonoma: "2c02c59135e6da59714ee09822b2e95510d455c9ef57e2dbc559e1614140394b"
+    sha256 cellar: :any_skip_relocation, ventura: "2c02c59135e6da59714ee09822b2e95510d455c9ef57e2dbc559e1614140394b"  end
 end
